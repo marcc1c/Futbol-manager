6e612713ae21b98a21b3da77f348a583c284e8e8
marcc1c <mgarciam2324@politecnics.barcelona>
2026-02-09 11:32:53 +0100

    Creadas clases personas, Jugador, Entrenador pero el push bien

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import personas.Entrenador;
+import personas.Jugador;
+import personas.Personas;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Personas p1 = new Personas("a", "b", "12121", 12, 200);
+        Jugador j1 = new Jugador("a", "b", "12121", 12, 200, 12, "DEL", 99);
+        Entrenador e1 = new Entrenador("a", "b", "12121", 12, 10110, 2, true);
+
+
+
+    }
+}
