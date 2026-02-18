4dfc9958f38a9cd4c03b9848b7b937fb8a5187b6
marcc1c <mgarciam2324@politecnics.barcelona>
2026-02-18 13:55:01 +0100

    Create Validador.java

---
+++ Aplicacio/src/Validador.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Validador {
+
+    public static int numerosInicioFinal(int numeroInicial, int numeroFinal) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.hasNextInt();
+
+        int input = 0;
+
+        boolean valido = false;
+        while (!valido) {
+            input = scanner.nextInt();
+            if (input < numeroInicial || input > numeroFinal) {
+                System.out.println("Valor incorrecto");
+            } else {
+                valido = true;
+            }
+        }
+        return input;
+    }
+
+    public static String arraylist(ArrayList<String> arrayValoresPosibles) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        boolean valido = false;
+        while (!valido) {
+            input = scanner.nextLine();
+            for (String palabras : arrayValoresPosibles) {
+                if (palabras.equalsIgnoreCase(input)) {
+                    valido = true;
+                }
+            }
+            if (!valido) {
+                System.out.println("Valor incorrecto");
+            }
+        }
+        return input;
+    }
+
+
+    public static String numero1(String numero) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        boolean valido = false;
+
+        while (!valido) {
+            input = scanner.nextLine();
+            if (input.equals(numero)) {
+                valido = true;
+            } else  {
+                System.out.println("Valor incorrecto");
+            }
+        }
+        return input;
+    }
+
+    public static String numero2(String numero1, String numero2) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        boolean valido = false;
+
+        while (!valido) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase(numero1) || input.equalsIgnoreCase(numero2)) {
+                valido = true;
+            } else  {
+                System.out.println("Valor incorrecto");
+            }
+        }
+        return input;
+    }
+
+    public static String numero3(String numero1, String numero2, String numero3) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        boolean valido = false;
+
+        while (!valido) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase(numero1) || input.equalsIgnoreCase(numero2) || input.equalsIgnoreCase(numero3)) {
+                valido = true;
+            } else  {
+                System.out.println("Valor incorrecto");
+            }
+        }
+        return input;
+    }
+}
+
