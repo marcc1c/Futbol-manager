80df78a01e042d9af8d95281fbdaefd4e281a867
marcc1c <mgarciam2324@politecnics.barcelona>
2026-02-18 12:07:40 +0100

    Menu funcional

--- Aplicacio/src/Main.java
+++ Aplicacio/src/Main.java
@@ -2,6 +2,9 @@ import personas.Entrenador;
 import personas.Jugador;
 import personas.Personas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,30 +14,43 @@ public class Main {
         Jugador j1 = new Jugador("a", "b", "12121", 12, 200, 12, "DEL", 99);
         Entrenador e1 = new Entrenador("a", "b", "12121", 12, 10110, 2, true);
 
-        char tipoUsuario = login();
-        String input = mostrarMenu(tipoUsuario);
+        String[] opcionesAdmin = new String[]{
+                "VC", "AE", "AJ", "DA", "DJ", "DV", "SE", "DD", "S"
+        };
+
+        String[] opcionesGestorEquipos = new String[]{
+                "VA", "GE", "DA", "DJ", "TJ", "DD", "S"
+        };
+
+        ArrayList<String> opcionesUsuarioActual = new ArrayList<>();
+
+        char tipoUsuario = login(opcionesAdmin, opcionesGestorEquipos, opcionesUsuarioActual);
+        String input = mostrarMenu(tipoUsuario, opcionesUsuarioActual);
         escogerOpcion(input, tipoUsuario);
 
     }
 
-    public static char login() {
+    public static char login(String[] opcionesAdmin, String[] opcionesGestorEquipos, ArrayList<String> opcionesUsuarioActual) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("-".repeat(15) + "Iniciando login" + "-".repeat(15));
         System.out.println("¿Eres admin (a) o eres un gestor de equipo (g)?");
         scanner.hasNextLine();
         String input = scanner.nextLine();
+
         char tipousuario = input.charAt(0);
         if (input.equalsIgnoreCase("a")) {
             tipousuario = 'a';
+            opcionesUsuarioActual.addAll(Arrays.asList(opcionesAdmin));
         } else if (input.equalsIgnoreCase("g")) {
             tipousuario = 'g';
+            opcionesUsuarioActual.addAll(Arrays.asList(opcionesGestorEquipos));
         } else {
 
         }
         return tipousuario;
     }
 
-    public static String mostrarMenu(char tipoUsuario) {
+    public static String mostrarMenu(char tipoUsuario, ArrayList<String> opcionesUsuarioActual) {
         String[] tipoMenu = {};
         String[] menuAdmin = {
                 "Welcome to Politècnics Football Manager:",
@@ -69,67 +85,73 @@ public class Main {
             System.out.println(linea);
 
         }
-        return pedirinput(tipoMenu.length - 2);
+        return pedirinput(opcionesUsuarioActual);
 
     }
 
-    public static String pedirinput(int longitud) {
+    public static String pedirinput(ArrayList<String> opcionesUsuario) {
         Scanner scanner = new Scanner(System.in);
         boolean valido = false;
         String input = "";
-/**
+
         while (!valido) {
-            input = scanner.nextInt();
-            if (input > 0 && input < longitud) {
+            input = scanner.nextLine();
+
+            boolean encontrado = false;
+
+                for (String opcion : opcionesUsuario) {
+                    if (opcion.equalsIgnoreCase(input)) {
+                        encontrado = true;
+                    }
+            }
+            if (encontrado) {
                 valido = true;
             } else {
-                System.out.println("Valor incorrecto, intoduce entre 0 y " + longitud);
+                System.out.println("Opción inválida");
             }
         }
-        **/
-        input = scanner.nextLine();
-
         return input;
     }
 
     public static void escogerOpcion(String input, char tipoUsuario) {
-            switch (input) {
-                case "S":
-                    break;
-                case "VC":
-                    break;
-                case "AE":
-                    break;
-                case "AJ":
-                    break;
-                case "DA":
-                    break;
-                case "DJ":
-                    break;
-                case "DV":
-                    break;
-                case "SE":
-                    break;
-                case "DD":
-                    break;
-                case "GE":
-                    getstionarMiEquipo();
-                    break;
-                case "TJ":
-                    break;
-                default:
-                    System.out.println("Valor incorrecto");
-                    break;
-            }
+        switch (input) {
+            case "S":
+                break;
+            case "VC":
+                break;
+            case "AE":
+                break;
+            case "AJ":
+                break;
+            case "DA":
+                break;
+            case "DJ":
+                break;
+            case "DV":
+                break;
+            case "SE":
+                break;
+            case "DD":
+                break;
+            case "GE":
+                getstionarMiEquipo();
+                break;
+            case "TJ":
+                break;
+            default:
+                System.out.println("Valor incorrecto");
+                break;
         }
-        public static void getstionarMiEquipo () {
-            System.out.println("Team Manager:");
-            System.out.println("1- Donar de baixa l'equip");
-            System.out.println("2- Modificar president/a");
-            System.out.println("3- Destituir entrenador/a");
-            System.out.println("4- Fitxar jugador/a o entrenador/a");
-            System.out.println("0- Sortir");
+    }
 
-        }
+    public static void getstionarMiEquipo() {
+        System.out.println("Team Manager:");
+        System.out.println("1- Donar de baixa l'equip");
+        System.out.println("2- Modificar president/a");
+        System.out.println("3- Destituir entrenador/a");
+        System.out.println("4- Fitxar jugador/a o entrenador/a");
+        System.out.println("0- Sortir");
 
     }
+
+}
