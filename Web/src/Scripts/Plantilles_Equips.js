0603e14446dd8705532c71b9fbd800a1c9910dbb
Roc <rburnate2324@politecnics.barcelona>
2026-02-26 13:55:25 +0100

    JS hecho

--- Web/src/Scripts/Plantilles_Equips.js
+++ Web/src/Scripts/Plantilles_Equips.js
@@ -1,73 +1,65 @@
-RutaEquip = "";
+console.log("Script carregat correctament");
+alert("JS s'ha carregat!"); 
 
+let EquipJSON = [];
+const contenidor = document.querySelector(".targetes-jugadors");
 const select = document.getElementById("Desplegable_Equips");
 
-select.addEventListener("change", function() {
-  const EquipSeleccionat = this.value;
-})
+fetch("../src/JSON/jugadorsMasculins.json")
+  .then(response => {
+      if (!response.ok) throw new Error("Fitxer JSON no trobat: " + response.status);
+      return response.json();
+  })
+  .then(data => {
+      EquipJSON = data;
+      console.log("JSON carregat:", EquipJSON);
 
-switch (EquipSeleccionat) {
-    case "FC Barcelona":
-        RutaEquip = "../Jugadors-es/JugadoresBarca";
-        break;
-    case "Real Madrid CF":
-        RutaEquip = "../Jugadors-es/JugadoresMadrid";
-        break;
-    case "Atlético de Madrid":
-        RutaEquip = "../Jugadors-es/JugadoresAtleticoMad";
-        break;
-    case "Sevilla FC":
-        RutaEquip = "../Jugadors-es/JugadoresSevilla";
-        break;
-    case "Real Sociedad":
-        RutaEquip = "../Jugadors-es/JugadoresRealSociedad";
-        break;
-    case "Real Betis":
-        RutaEquip = "../Jugadors-es/JugadoresBetis";
-        break;
-    case "Athletic Club":
-        RutaEquip = "../Jugadors-es/JugadoresAthletic";
-        break;
-    case "Villarreal CF":
-        RutaEquip = "../Jugadors-es/JugadoresVillarreal";
-        break;
-    case "Valencia CF":
-        RutaEquip = "../Jugadors-es/JugadoresValencia";
-        break;
-    case "RC Celta de Vigo":
-        RutaEquip = "../Jugadors-es/JugadoresCeltaDeVigo";
-        break;
-    case "RCD Mallorca":
-        RutaEquip = "../Jugadors-es/JugadoresMallorca";
-        break;
-    case "Girona FC":
-        RutaEquip = "../Jugadors-es/JugadoresGirona";
-        break;
-    case "CA Osasuna":
-        RutaEquip = "../Jugadors-es/JugadoresOsasuna";
-        break;
-    case "Getafe CF":
-        RutaEquip = "../Jugadors-es/JugadoresGetafe";
-        break;
-    case "UD Las Palmas":
-        RutaEquip = "../Jugadors-es/JugadoresLaspalmas";
-        break;
-    case "RCD Espanyol":
-        RutaEquip = "../Jugadors-es/JugadoresEspañol";
-        break;
-    case "CD Leganés":
-        RutaEquip = "../Jugadors-es/JugadoresLeganes";
-        break;
-    case "Real Valladolid CF":
-        RutaEquip = "../Jugadors-es/JugadoresValladolid";
-        break;
-    case "Rayo Vallecano":
-        RutaEquip = "../Jugadors-es/JugadoresRayoVallecano";
-        break;
-    case "Deportivo Alavés":
-        RutaEquip = "../Jugadors-es/JugadoresAlaves";
-        break;
-    default:
-        RutaEquip = "";
-        break;
-}
+      select.addEventListener("change", function() {
+          if (EquipJSON.length === 0) {
+              alert("JSON encara no carregat, intenta-ho en un moment");
+              return;
+          }
+
+          const EquipSeleccionat = this.value;
+          contenidor.innerHTML = "";
+
+          if (!EquipSeleccionat) return;
+
+          const EquipObj = EquipJSON.find(e => e.equip === EquipSeleccionat);
+          if (!EquipObj) {
+              alert("Equip no trobat: " + EquipSeleccionat);
+              return;
+          }
+
+          EquipObj.jugadors.forEach(jugador => {
+              const targeta = document.createElement("div");
+              targeta.classList.add("targeta-jugador");
+
+              const imgFondo = document.createElement("img");
+              imgFondo.classList.add("imatge-fondo");
+              imgFondo.src = "../src/img/pngtree-empty-gold-player-card-rating-vector-png-image_12575430.png";
+              targeta.appendChild(imgFondo);
+
+              const imgJugador = document.createElement("img");
+              imgJugador.classList.add("imatge-a-dalt");
+              imgJugador.src = jugador.foto || "../src/img/Persona Default.png";
+              targeta.appendChild(imgJugador);
+
+              const nom = document.createElement("h3");
+              nom.classList.add("Nom");
+              nom.textContent = jugador.nomPersona;
+              targeta.appendChild(nom);
+
+              const info = document.createElement("p");
+              info.classList.add("info");
+              info.innerHTML = `Dorsal: ${jugador.dorsal}<br>Posició: ${jugador.posicio}<br>Qualitat: ${jugador.qualitat}`;
+              targeta.appendChild(info);
+
+              contenidor.appendChild(targeta);
+          });
+      });
+  })
+  .catch(error => {
+      alert("Error carregant JSON: " + error.message);
+      console.error(error);
+  });
